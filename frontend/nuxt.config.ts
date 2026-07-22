549f7a7594f8e50562c38c3fc947d326ae2d1552
amirtalbi <amirotalbi94@gmail.com>
2026-07-23 01:03:36 +0200

    feat: set values for server

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: "http://localhost:3001",
+      apiBase: "mktp.amirtalbi.fr/api",
     },
   },
   modules: ["@nuxt/ui", "@pinia/nuxt"],
